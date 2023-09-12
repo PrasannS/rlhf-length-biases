61e701657d1448c8b698881cf55de45f6b26138b
Prasann Singhal <prasannsinghal.dev@gmail.com>
2023-09-12 08:40:29 -0500

    scripts denali

--- trl-general/run_rlhf.sh
+++ trl-general/run_rlhf.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0,1
 
 # NOTE that multiple runs needs multiple device ids / whatnot
 
@@ -13,15 +13,15 @@ accelerate launch --multi_gpu --config_file=default_config.yaml \
     train_rlhf.py --log_with=wandb \
     --model_name=/mnt/data1/prasann/rlhf-exploration/webgpt-llama/models/sft10k \
     --dataset_name="rlcd" \
-    --reward_model_name=/mnt/data1/prasann/rlhf-exploration/rlcd-llama/models/rlcdmix20 \
+    --reward_model_name=/mnt/data1/prasann/rlhf-exploration/rlcd-llama/models/rlcdgoodtrunc \
     --adafactor=False \
     --save_freq=25 \
     --output_max_length=156 --batch_size=32 \
     --gradient_accumulation_steps=1 \
     --ppo_epochs=1 --seed=0 --learning_rate=1.4e-5 \
-    --early_stopping=False --output_dir=checkpoints/rlcdmix20/ \
-    --init_kl_coef=0.04 --steps=1000 \
-    --reward_baseline=1.5
+    --early_stopping=False --output_dir=checkpoints/rlcdgoodtruncppo/ \
+    --init_kl_coef=0.04 --steps=1000
+    #--reward_baseline=1.5
 
 # WebGPT Job
 # accelerate launch --multi_gpu --config_file=default_config.yaml \
