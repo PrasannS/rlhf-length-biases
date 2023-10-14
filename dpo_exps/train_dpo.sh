6712731014d1cce2490af7eb747fa9cab7787d41
PrasannSinghal <prasannsinghal.dev@gmail.com>
2023-10-14 20:48:41 +0000

    prepping DPO experiments

--- dpo_exps/train_dpo.sh
+++ dpo_exps/train_dpo.sh
@@ -1,9 +1,9 @@
-# DPO setup for WebGPT
-export CUDA_VISIBLE_DEVICES=0,1
-accelerate launch --multi_gpu --config_file=../scripts/default_config.yaml \
-    examples/stack_llama_2/scripts/dpo_llama2.py \
-    --model_name_or_path="../models/sft10k" --output_dir="dpo" \
-    --dataset="wgpt" --log_with="wandb" 
+# # DPO setup for WebGPT
+# export CUDA_VISIBLE_DEVICES=2,3
+# accelerate launch --multi_gpu --config_file=../scripts/default_config.yaml \
+#     examples/stack_llama_2/scripts/dpo_llama2.py \
+#     --model_name_or_path="../models/sft10k" --output_dir="dpo" \
+#     --dataset="wgpt" --log_with="wandb" 
 
 # DPO setup for Stack
 export CUDA_VISIBLE_DEVICES=0,1
@@ -12,9 +12,9 @@ accelerate launch --multi_gpu --config_file=../scripts/default_config.yaml \
     --model_name_or_path="../models/sft10k" --output_dir="dpo" \
     --dataset="wgpt" --log_with="wandb" 
 
-# DPO setup for RLCD
-export CUDA_VISIBLE_DEVICES=4,5
-accelerate launch --multi_gpu --config_file=../scripts/default_config.yaml \
-    examples/stack_llama_2/scripts/dpo_llama2.py \
-    --model_name_or_path="../models/sft10k" --output_dir="dpo" \
-    --dataset="wgpt" --log_with="wandb" 
+# # DPO setup for RLCD
+# export CUDA_VISIBLE_DEVICES=4,5
+# accelerate launch --multi_gpu --config_file=../scripts/default_config.yaml \
+#     examples/stack_llama_2/scripts/dpo_llama2.py \
+#     --model_name_or_path="../models/sft10k" --output_dir="dpo" \
+#     --dataset="wgpt" --log_with="wandb" 
