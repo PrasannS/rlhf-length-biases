a339a71dc660d9eaaee083f2eddfa197c70b43a2
PrasannSinghal <prasannsinghal.dev@gmail.com>
2023-11-14 15:22:17 +0000

    script change

--- train_rm.sh
+++ train_rm.sh
@@ -19,7 +19,7 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 torchrun --nnodes 1  --nproc_per_node 4 --master_port=12337 scripts/train_rm.py \
         --model_name=/u/prasanns/research/rlhf-length-biases/models/llama \
-        --output_dir=checkpoints/ultrafeed/prefoverprefrm \
+        --output_dir=checkpoints/ultranormal \
         --dataset="ultra" \
         --rand_ratio=0 \
         --balance_len=0 \
