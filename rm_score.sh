fa2a7c3c51d9a709e40e5615e522efe716543683
PrasannSinghal <prasannsinghal.dev@gmail.com>
2023-11-16 20:00:53 +0000

    GPU efficiency engineering trick

--- rm_score.sh
+++ rm_score.sh
@@ -13,11 +13,13 @@
 #     --lim 600 \
 #     --shuffle 0
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 python -u scripts/rmsco_outs.py \
-    --rmname="/u/prasanns/research/rlhf-length-biases/models/rewards/harmrm" \
-    --inpf="/u/prasanns/research/rlhf-length-biases/outputs/multisamp/generated_rlcdsftmulti.jsonl" \
-    --device 2 \
+    --rmname="/home/prasann/Projects/rlhf-length-biases/models/rewards/harmrm" \
+    --inpf="/home/prasann/Projects/rlhf-exploration/scripts/genouts/multisamp/generated_rlcdsftmulti.jsonl" \
+    --device 0 \
     --lim 600 \
     --shuffle 0
+
+
 
